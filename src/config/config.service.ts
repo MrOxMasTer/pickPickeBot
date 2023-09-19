bd156bee7638bc8b02bce895f06df2d9c378b572
MrOxMasTer <59291123+MrOxMasTer@users.noreply.github.com>
2023-09-19 17:42:22 +0300

    attempt 2

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,28 +1,26 @@
 // import { DotenvParseOutput, config } from "dotenv";
-// import { IConfigService } from "./config.interface";
+import { IConfigService } from "./config.interface";
 
-// export class ConfigService implements IConfigService {
-//     private config: DotenvParseOutput;
+export class ConfigService implements IConfigService {
+    // private config: DotenvParseOutput;
 
-//     constructor() {
-//         const { parsed, error } = config();
+    constructor() {
+        // const { parsed, error } = config();
+        // if (error) {
+        //     throw new Error("Не найден файл .env");
+        // }
+        // if (!parsed) {
+        //     throw new Error("Пустой файл .env");
+        // }
+        // this.config = parsed;
+    }
 
-//         if (error) {
-//             throw new Error("Не найден файл .env");
-//         }
-//         if (!parsed) {
-//             throw new Error("Пустой файл .env");
-//         }
-
-//         this.config = parsed;
-//     }
-
-//     public get(key: string): string {
-//         const res = this.config[key];
-//         if (!res) {
-//             throw new Error("Нет такого ключа");
-//         }
-
-//         return res;
-//     }
-// }
+    public get(key: string): string {
+        // const res = this.config[key];
+        // if (!res) {
+        //     throw new Error("Нет такого ключа");
+        // }
+        return key;
+        // return res;
+    }
+}
